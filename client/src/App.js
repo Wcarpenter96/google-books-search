aa96005b3fa6128c6d60cd8755c429450ac4656d
Weston Carpenter <wc.cc96@gmail.com>
2019-11-06 09:00:57 -0800

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,104 @@
+import React, { Component } from "react";
+import Jumbotron from "./components/Jumbotron";
+import Nav from "./components/Nav";
+import Input from "./components/Input";
+import Button from "./components/Button";
+import API from "./utils/API";
+import { BookList, BookListItem } from "./components/BookList";
+import { Container, Row, Col } from "./components/Grid";
+
+class App extends Component {
+  state = {
+    books: [],
+    bookSearch: ""
+  };
+
+  handleInputChange = event => {
+    // Destructure the name and value properties off of event.target
+    // Update the appropriate state
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // When the form is submitted, prevent its default behavior, get recipes update the recipes state
+    event.preventDefault();
+    API.getRecipes(this.state.bookSearch)
+      .then(res => {
+        console.log(res.data);
+        this.setState({  books: res.data });
+      })
+      .catch(err => console.log(err));
+  };
+
+  renderBookList = () => {
+    if (this.state.books.length < 1) {
+      return <h1>No books found</h1>
+    } else {
+      return (
+        <BookList>
+          {
+            this.state.books.map((book, i) => {
+              return (
+                <BookListItem key={i}
+                  url={book.href}
+                  ingredients={book.ingredients}
+                  thumbnail={book.thumbnail}
+                  title={book.title}>
+                </BookListItem>
+              )
+            })
+          }
+        </BookList>
+      )
+    }
+  }
+
+  render() {
+    console.log(this.state.book)
+    return (
+      <div>
+        <Nav />
+        <Jumbotron />
+        <Container>
+          <Row>
+            <Col size="md-12">
+              <form>
+                <Container>
+                  <Row>
+                    <Col size="xs-9 sm-10">
+                      <Input
+                        name="bookSearch"
+                        value={this.state.bookSearch}
+                        onChange={this.handleInputChange}
+                        placeholder="Search For a Book"
+                      />
+                    </Col>
+                    <Col size="xs-3 sm-2">
+                      <Button
+                        onClick={this.handleFormSubmit}
+                        type="success"
+                        className="input-lg"
+                      >
+                        Search
+                      </Button>
+                    </Col>
+                  </Row>
+                </Container>
+              </form>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="xs-12">
+              {this.renderBookList()}
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/BookList/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Thumbnail from "../Thumbnail";
+import { Container, Row, Col } from "../Grid";
+
+export function BookList ({ children }) {
+  return <ul className="list-group">{children}</ul>;
+}
+
+export function BookListItem (props) {
+  return (
+    <li className="list-group-item">
+      <Container>
+        <Row>
+          <Col size="xs-4 sm-2">
+            <Thumbnail src={props.thumbnail} />
+          </Col>
+          <Col size="xs-8 sm-9">
+            <h3>{props.title}</h3>
+            <p>
+              Summary: "{props.ingredients}"
+            </p>
+            <a
+              rel="noreferrer noopener"
+              target="_blank"
+              href={props.url}
+            >
+              Go to book!
+            </a>
+          </Col>
+        </Row>
+      </Container>
+    </li>
+  );
+}
